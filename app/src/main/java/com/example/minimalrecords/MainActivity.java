d95e9b8bc943a9ef700a8ffb7ab8adf77375a4b9
roman <romanze1706@gmail.com>
2023-01-21 20:01:08 +0300

    Создал необходимые страницы

--- app/src/main/java/com/example/minimalrecords/MainActivity.java
+++ app/src/main/java/com/example/minimalrecords/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.minimalrecords;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToUs(View view) {
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+    }
+    public void goToPhoto(View view) {
+        Intent intent = new Intent(this, Photo.class);
+        startActivity(intent);
+    }
+    public void goToBuild(View view) {
+        Intent intent = new Intent(this, Build.class);
+        startActivity(intent);
+    }
+    public void goToApparatura(View view) {
+        Intent intent = new Intent(this, Apparatura.class);
+        startActivity(intent);
+    }
+    public void goToPortfolio(View view) {
+        Intent intent = new Intent(this, Portfolio.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/minimalrecords/Portfolio.java
@@ -0,0 +1,14 @@
+package com.example.minimalrecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Portfolio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portfolio);
+    }
+}
